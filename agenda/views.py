080198025f4847135be9cb659c019c776243b3f4
paulafalves <paulafernandaalves@icloud.com>
2022-03-18 17:15:35 -0300

    create post method

--- agenda/views.py
+++ agenda/views.py
@@ -11,10 +11,27 @@ def agendamento_detail(request, id):
 
   return JsonResponse(serializer.data)
 
-@api_view(http_method_names=["GET"])
+@api_view(http_method_names=["GET", "POST"])
 def agendamento_list(request):
-  qs = Agendamento.objects.all()
-  serializer = AgendamentoSerializer(qs, many=True)
+  if request.method == 'GET':
+    qs = Agendamento.objects.all()
+    serializer = AgendamentoSerializer(qs, many=True)
 
-  return JsonResponse(serializer.data, safe=False)
+    return JsonResponse(serializer.data, safe=False)
 
+  if request.method == "POST":
+    data = request.data
+    serializer = AgendamentoSerializer(data=data)
+
+    if serializer.is_valid():
+      valitated_data = serializer.validated_data
+      Agendamento.objects.create(
+        data_horario=valitated_data["data_horario"],
+        nome_cliente=valitated_data["nome_cliente"],
+        email_cliente=valitated_data["email_cliente"],
+        telefone_cliente=valitated_data["telefone_cliente"],
+      )
+
+      return JsonResponse(serializer.data, status=201)
+
+    return JsonResponse(serializer.errors, status=400)
