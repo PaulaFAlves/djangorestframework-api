bd2cd4990625fa01e98ec6a902b0f26013ac7060
paulafalves <paulafernandaalves@icloud.com>
2022-03-23 16:33:19 -0300

    include tests

--- agenda/tests.py
+++ agenda/tests.py
@@ -1,31 +1,66 @@
-from django.test import TestCase, Client
+from datetime import datetime
+import json
+from django.forms import ValidationError
+from django.test import Client
+from rest_framework.test import APITestCase
 from agenda.models import Agendamento
-from django.utils import timezone
+from django.utils import timezone 
+from rest_framework import serializers
 
 
 
-class TestAgendamento(TestCase):
+
+class TestAgendamento(APITestCase):
+  def test_listagem_vazia(self):
+    response = self.client.get('/api/agendamentos/')
+    data = json.loads(response.content)
+    self.assertEqual(data, [])
+
   def test_lista_agendamentos(self):
+    agendamentos_esperados = [{
+        'id': 1, 
+        'data_horario': '2022-12-12T00:00:00Z', 
+        'nome_cliente': 'PAula', 
+        'email_cliente': 'paula@email.com', 
+        'telefone_cliente': '444'}, 
+      {
+        'id': 2, 
+        'data_horario': '2023-12-12T00:00:00Z', 
+        'nome_cliente': 'Carol', 
+        'email_cliente': 'paula@email.com', 
+        'telefone_cliente': '444'
+        }]
+
+    Agendamento.objects.create(data_horario=datetime(2022, 12, 12), nome_cliente="PAula", email_cliente="paula@email.com", telefone_cliente="444")
+    Agendamento.objects.create(data_horario=datetime(2023, 12, 12), nome_cliente="Carol", email_cliente="paula@email.com", telefone_cliente="444")
+
     client = Client()
     response = client.get('/api/agendamentos/')
-    self.assertEqual(response.status_code, 200)
+    data = json.loads(response.content)
+
+    self.assertEqual(len(data), 2)
+    self.assertEqual(data, agendamentos_esperados)
+
+  def test_quanto_request_retorna_400(self):
+    Agendamento.objects.create(data_horario=datetime(2022, 12, 12))
+    self.assertRaises(ValidationError)
 
   def test_cria_agendamento(self):
-    agendamento = Agendamento.objects.create(data_horario=timezone.now(), nome_cliente="PAula", email_cliente="paula@email.com", telefone_cliente="444")
+    Agendamento.objects.create(data_horario=timezone.now(), nome_cliente="PAula", email_cliente="paula@email.com", telefone_cliente="444")
 
     client = Agendamento.objects.get(nome_cliente="PAula")
 
     self.assertEqual(client.nome_cliente, "PAula")
 
   def test_agendamento_tem_atributo_is_canceled_false_quando_criado(self):
-    agendamento = Agendamento.objects.create(data_horario=timezone.now(), nome_cliente="PAula", email_cliente="paula@email.com", telefone_cliente="444")
+    Agendamento.objects.create(data_horario=timezone.now(), nome_cliente="PAula", email_cliente="paula@email.com", telefone_cliente="444")
 
     client = Agendamento.objects.get(nome_cliente="PAula")
 
     self.assertFalse(client.is_canceled)
 
   def test_atributo_is_canceled_true_quando_agendamento_cancelado(self):
-    agendamento = Agendamento.objects.create(data_horario=timezone.now(), nome_cliente="PAula", email_cliente="paula@email.com", telefone_cliente="444")
+    Agendamento.objects.create(data_horario=timezone.now(), nome_cliente="PAula", email_cliente="paula@email.com", telefone_cliente="444")
 
     client = Agendamento.objects.get(nome_cliente="PAula")
     response = self.client.delete(f'/api/agendamentos/{client.id}/')
