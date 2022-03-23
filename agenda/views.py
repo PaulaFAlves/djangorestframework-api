e6fce85994fd3e456b8357441f5b966ba94a1eb9
paulafalves <paulafernandaalves@icloud.com>
2022-03-23 17:56:12 -0300

    refactor-to-class-based-views

--- agenda/views.py
+++ agenda/views.py
@@ -1,22 +1,22 @@
 import re
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from agenda.models import Agendamento
 from agenda.serializers import AgendamentoSerializer
 
-@api_view(http_method_names=["GET", "PATCH", "DELETE"])
-def agendamento_detail(request, id):
-  obj = get_object_or_404(Agendamento, id=id)
-
-  if request.method == "GET":
+class AgendamentoDetail(APIView):
+  def get(self, request):
+    obj = get_object_or_404(Agendamento, id=id)
     serializer = AgendamentoSerializer(obj)
 
     return JsonResponse(serializer.data)
-  
-  if request.method == "PATCH":
+
+  def patch(self, request):
     data = request.data
+    obj = get_object_or_404(Agendamento, id=id)
     serializer = AgendamentoSerializer(obj, data=data, partial=True)
 
     if serializer.is_valid():
@@ -26,21 +26,21 @@ def agendamento_detail(request, id):
     
     return JsonResponse(serializer.errors, status=400)
 
-  if request.method == "DELETE":
+  def delete(self, request, id):
+    obj = get_object_or_404(Agendamento, id=id)
     obj.is_canceled = True
     obj.save()
 
     return Response(status=204)
 
-@api_view(http_method_names=["GET", "POST"])
-def agendamento_list(request):
-  if request.method == 'GET':
+class AgendamentoList(APIView):
+  def get(self, request):
     qs = Agendamento.objects.all().filter(is_canceled=False)
     serializer = AgendamentoSerializer(qs, many=True)
 
     return JsonResponse(serializer.data, safe=False)
 
-  if request.method == "POST":
+  def post(self, request):
     data = request.data
     serializer = AgendamentoSerializer(data=data)
 
