5d9fc46a57a2d4e8ab6e05312ed6150b862cedc3
paulafalves <paulafernandaalves@icloud.com>
2022-04-02 12:04:29 -0300

    --apend

--- agenda/views.py
+++ agenda/views.py
@@ -1,8 +1,14 @@
+from django.http import HttpResponse, JsonResponse
 from agenda.models import Agendamento
-from agenda.serializers import AgendamentoSerializer, PrestadorSerializer
+from agenda.serializers import AgendamentoSerializer, HorariosSerializer, PrestadorSerializer
 from rest_framework import permissions
 from rest_framework import generics
 from django.contrib.auth.models import User
+from datetime import datetime
+from django.utils import timezone
+from rest_framework.decorators import api_view
+
+from agenda.utils import get_horarios_disponiveis
 
 class IsOwnerOrCreateOnly(permissions.BasePermission):
   def has_permission(self, request, view):
@@ -42,4 +48,13 @@ class PrestadorList(generics.ListAPIView):
   serializer_class = PrestadorSerializer
   queryset = User.objects.all()
 
-  
+@api_view(http_method_names=["GET"])
+def get_horarios(request):
+  data = request.query_params.get("data")
+  if not data:
+    data = datetime.now().date()
+  else:
+    data = datetime.fromisoformat(data).date()
+
+  horarios_disponiveis = sorted(list(get_horarios_disponiveis(data)))
+  return JsonResponse(horarios_disponiveis, safe=False)
