fc7bf54f2908606e5bf35ebcd881f6473390db94
paulafalves <paulafernandaalves@icloud.com>
2022-04-11 21:46:25 -0300

    create csv file

--- agenda/urls.py
+++ agenda/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from agenda.views import AgendamentoDetail, AgendamentoList, PrestadorList, get_horarios, healthcheck
+from agenda.views import AgendamentoDetail, AgendamentoList, get_horarios, healthcheck, relatorio_prestadores
 
 urlpatterns = [
      path('agendamentos/', AgendamentoList.as_view()),
      path('agendamentos/<int:pk>/', AgendamentoDetail.as_view()),
-     path('prestadores/', PrestadorList.as_view()),
+     path('prestadores/', relatorio_prestadores),
      path('horarios/', get_horarios),
      path('', healthcheck)
 ]
